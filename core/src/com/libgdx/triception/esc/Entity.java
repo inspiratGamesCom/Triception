d9b37c0f121b2e5535291228437dafeabc6c5e4b
AdriaBosch <adria.bosk@gmail.com>
2017-02-03 18:59:01 +0100

    prueba

--- core/src/com/libgdx/triception/esc/Entity.java
+++ core/src/com/libgdx/triception/esc/Entity.java
@@ -16,6 +16,9 @@ public class Entity {
 	private static final String TAG = Entity.class.getSimpleName();
 	private Json _json;
 
+
+
+
 	private EntityConfig _entityConfig;
 
 	public static enum Direction {
